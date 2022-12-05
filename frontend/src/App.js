3d13f33a68f422deec9ab58f7dc20f3fa0f3ff71
Danny Cheun <dcheun@gmail.com>
2022-12-05 16:56:00 +0800

    chore: Frontend: Update packages, Formatting
    Update package dependencies, and some auto formatting.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './stylesheets/App.css';
-import FormView from './components/FormView';
-import QuestionView from './components/QuestionView';
-import Header from './components/Header';
-import QuizView from './components/QuizView';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./stylesheets/App.css";
+import FormView from "./components/FormView";
+import QuestionView from "./components/QuestionView";
+import Header from "./components/Header";
+import QuizView from "./components/QuizView";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Header path />
         <Router>
-          <Switch>
-            <Route path='/' exact component={QuestionView} />
-            <Route path='/add' component={FormView} />
-            <Route path='/play' component={QuizView} />
-            <Route component={QuestionView} />
-          </Switch>
+          <Routes>
+            <Route path="/" exact element={<QuestionView />} />
+            <Route path="/add" element={<FormView />} />
+            <Route path="/play" element={<QuizView />} />
+            <Route element={<QuestionView />} />
+          </Routes>
         </Router>
       </div>
     );
